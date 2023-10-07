26a27e46ad4a99669b8072bb57efe5a93e8571e4
aschelberg <39310235+aschelberg@users.noreply.github.com>
2023-10-07 15:39:22 +0000

    Update index-functions.js and index.mjs

--- src/index-functions.js
+++ src/index-functions.js
@@ -24,83 +24,16 @@ const saveTodos = function (todos) {
 };
 
 // Save deleted todos to local storage
-const saveDeletedTodos = function (deletedTodos, todos) {
+const saveDeletedTodos = function (deletedTodos) {
   localStorage.setItem("deletedTodos", JSON.stringify(deletedTodos));
 };
 
-// Generate the DOM structure for a todo item
-// const generateTodoDOM
-
-// Update object value and save to local storage
-const completeTodo = function (index) {
-  const todo = todos.find((todo, i) => index === i);
-  todo.completed = true;
-  saveTodos(todos);
-};
-
-// update deletedTodos array in local storage, Needs to remove from todos
-const deleteTodo = function (index) {
-  const removeTodo = todos.splice(index, 1);
-  deletedTodos.push(removeTodo);
-  saveDeletedTodos(deletedTodos);
-  renderTodos(todos, filters);
-};
-
-// generate individual button for list item
-const generateButton = function (icon) {
-  const button = document.createElement("button");
-  button.classList.add("todo-list-item-button");
-  button.innerHTML = icon;
-
-  return button;
-};
-
-// generates both buttons and adds to new todo list item
-const generateButtons = function (index) {
-  const buttonWrapper = document.createElement("div");
-  buttonWrapper.classList.add("todo-list-item-icons");
-  const completeButton = generateButton(completeIcon);
-  const deleteButton = generateButton(deleteIcon);
-
-  completeButton.addEventListener("click", function () {
-    completeTodo(index);
-  });
-  deleteButton.addEventListener("click", function () {
-    deleteTodo(index);
-  });
-
-  buttonWrapper.appendChild(completeButton);
-  buttonWrapper.appendChild(deleteButton);
-
-  return buttonWrapper;
-};
-
 // Generate DOM structure for todo list item
-const generateTodoDOM = function (todo, index) {
-  const todoEl = document.createElement("li");
-  todoEl.classList.add("todo-list-item");
-  todoEl.textContent = todo.text;
-  const buttons = generateButtons(index);
-  todoEl.appendChild(buttons);
-  return todoEl;
-};
-
-// Render todos based on filters
-const renderTodos = function (todos, filters) {
-  const filteredTodos = todos.filter(function (todo) {
-    const searchTextMatch = todo.text
-      .toLowerCase()
-      .includes(filters.searchText.toLowerCase());
-    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
-
-    return searchTextMatch && hideCompletedMatch;
-  });
-
-  document.querySelector(".todo-list").innerHTML = "";
-
-  filteredTodos.forEach(function (todo, index) {
-    document
-      .querySelector(".todo-list")
-      .appendChild(generateTodoDOM(todo, index));
-  });
-};
+// const generateTodoDOM = function (todo, index) {
+//   const todoEl = document.createElement("li");
+//   todoEl.classList.add("todo-list-item");
+//   todoEl.textContent = todo.text;
+//   const buttons = generateButtons(index);
+//   todoEl.appendChild(buttons);
+//   return todoEl;
+// };
--- src/index.mjs
+++ src/index.mjs
@@ -9,12 +9,80 @@ const filters = {
   hideCompleted: false,
 };
 
+// Update object value and save to local storage
+const completeTodo = function (index) {
+  const todo = todos.find((todo, i) => index === i);
+  todo.completed = true;
+  saveTodos(todos);
+};
+
+// update deletedTodos array in local storage, Needs to remove from todos
+const deleteTodo = function (index) {
+  const removeTodo = todos.splice(index, 1);
+  deletedTodos.push(removeTodo);
+  saveDeletedTodos(deletedTodos);
+  renderTodos(todos, filters);
+};
+
+// generate individual button for list item
+const generateButton = function (icon) {
+  const button = document.createElement("button");
+  button.classList.add("todo-list-item-button");
+  button.innerHTML = icon;
+
+  return button;
+};
+
+// generates both buttons and adds to new todo list item
+const generateButtons = function (index) {
+  const buttonWrapper = document.createElement("div");
+  buttonWrapper.classList.add("todo-list-item-icons");
+  const completeButton = generateButton(completeIcon);
+  const deleteButton = generateButton(deleteIcon);
+
+  completeButton.addEventListener("click", function () {
+    completeTodo(index);
+  });
+
+  deleteButton.addEventListener("click", function () {
+    deleteTodo(index);
+  });
+
+  buttonWrapper.appendChild(completeButton);
+  buttonWrapper.appendChild(deleteButton);
+
+  return buttonWrapper;
+};
+
+// Render todos based on filters
+const renderTodos = function (todos, filters) {
+  const filteredTodos = todos.filter(function (todo) {
+    const searchTextMatch = todo.text
+      .toLowerCase()
+      .includes(filters.searchText.toLowerCase());
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+    return searchTextMatch && hideCompletedMatch;
+  });
+
+  document.querySelector(".todo-list").innerHTML = "";
+
+  filteredTodos.forEach(function (todo, index) {
+    const todoEl = document.createElement("li");
+    todoEl.classList.add("todo-list-item");
+    todoEl.textContent = todo.text;
+    const buttons = generateButtons(index);
+    todoEl.appendChild(buttons);
+
+    document.querySelector(".todo-list").appendChild(todoEl);
+  });
+};
+
 renderTodos(todos, filters);
 
 searchTodo.addEventListener("input", function (e) {
   filters.searchText = e.target.value;
   renderTodos(todos, filters);
-  console.log(e.target.value);
 });
 
 addNewTodo.addEventListener("submit", function (e) {
